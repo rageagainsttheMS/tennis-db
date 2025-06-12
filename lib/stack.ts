6fd7f7c2212d8d34ec2f3a9c5f54f281a2978032
William Wrigley <william@bournedigital.com.au>
2025-06-12 14:49:31 +1000

    initial commit

---
+++ lib/stack.ts
@@ -0,0 +1,19 @@
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { NextjsGlobalFunctions } from "cdk-nextjs";
+import { join } from "node:path";
+
+
+
+class NextjsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    new NextjsGlobalFunctions(this, "Nextjs", {
+      healthCheckPath: "/api/health",
+      buildContext: join(__dirname, ".."),
+    });
+  }
+}
+
+const app = new App();
+new NextjsStack(app, 'nextjs');
